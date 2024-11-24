86840251fddd4081070bb68ddbdef7c9f7d7d2fd
Eduardo <eduardojosegonzalezmorales41@gmail.com>
2024-11-24 21:26:42 +0000

    Defining the basic structure for the template of the layout, that I'm going to use for all the pages of this blog.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,34 @@
 <template>
 <nav>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <div>
+        <header>
+            <nav>
+                <div>
+                    <div>
+                        <NuxtLink>
+
+                            Eduardo Morales - Nuxt Blog
+                        </NuxtLink>
+                        <div>
+                            <NuxtLink>Home</NuxtLink>
+                            <NuxtLink>Blog</NuxtLink>
+                            <NuxtLink>About</NuxtLink>
+                        </div>
+                    </div>
+                    <div>
+                        <ClientOnly>
+                            <template>
+                                <NuxtLink>Login</NuxtLink>
+                            </template>
+                            <template>
+                                <button @click="logout"></button>
+                            </template>
+                        </ClientOnly>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
 </nav>
 
 <slot/>
